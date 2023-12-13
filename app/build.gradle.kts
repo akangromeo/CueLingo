9fd86d438efa0d6606a5320e1e4c502471e0a444
akangromeo <deanagnia7@gmail.com>
2023-12-13 14:12:42 +0800

    update : implement tflite (still error)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,22 +45,18 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation("androidx.camera:camera-lifecycle:1.3.0")
-    implementation("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.camera:camera-view:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("androidx.camera:camera-core:1.1.0-beta01")
-    implementation ("androidx.camera:camera-camera2:1.1.0-beta01")
-
-    implementation ("org.tensorflow:tensorflow-lite:3.8.0")
+    implementation ("org.tensorflow:tensorflow-lite:2.4.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
     implementation ("org.tensorflow:tensorflow-lite-metadata:0.1.0")
-    // Import the GPU delegate plugin Library for GPU inference
-    implementation ("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.0")
-    implementation ("org.tensorflow:tensorflow-lite-gpu:2.9.0")
 
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+
 }
--- app/src/main/java/com/example/cuelingo/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/cuelingo/ui/camera/CameraActivity.kt
@@ -1,11 +1,15 @@
 package com.example.cuelingo.ui.camera
 
+//import androidx.camera.core.ImageAnalysis
+//import androidx.camera.core.ImageProxy
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
@@ -13,6 +17,7 @@ import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.util.Log
 import android.view.Surface
 import android.view.TextureView
 import android.widget.ImageView
@@ -20,16 +25,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.cuelingo.R
 import com.example.cuelingo.ml.ModelSignLanguage
+import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
 
 class CameraActivity : AppCompatActivity() {
 
-    lateinit var labels:List<String>
+
+    lateinit var labels: List<String>
     var colors = listOf<Int>(
         Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
-        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED)
-
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED
+    )
     val paint = Paint()
     lateinit var imageProcessor: ImageProcessor
     lateinit var bitmap: Bitmap
@@ -38,14 +47,16 @@ class CameraActivity : AppCompatActivity() {
     lateinit var handler: Handler
     lateinit var cameraManager: CameraManager
     lateinit var textureView: TextureView
-    lateinit var model:ModelSignLanguage
+    lateinit var model: ModelSignLanguage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         get_permission()
 
-        imageProcessor = ImageProcessor.Builder().add(ResizeOp(416, 416, ResizeOp.ResizeMethod.BILINEAR)).build()
+//        labels = FileUtil.loadLabels(this, "labels.txt")
+        imageProcessor =
+            ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
         model = ModelSignLanguage.newInstance(this)
         val handlerThread = HandlerThread("videoThread")
         handlerThread.start()
@@ -54,10 +65,11 @@ class CameraActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
 
         textureView = findViewById(R.id.textureView)
-        textureView.surfaceTextureListener = object: TextureView.SurfaceTextureListener{
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
             override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
                 open_camera()
             }
+
             override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
             }
 
@@ -67,6 +79,60 @@ class CameraActivity : AppCompatActivity() {
 
             override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
 
+                val bitmap = textureView.bitmap
+
+                val byteBuffer = ByteBuffer.allocateDirect(bitmap!!.byteCount)
+                bitmap.copyPixelsToBuffer(byteBuffer)
+                byteBuffer.rewind()
+
+                val inputFeature0 =
+                    TensorBuffer.createFixedSize(intArrayOf(1, 416, 416, 3), DataType.FLOAT32)
+                if (byteBuffer.capacity() == inputFeature0.flatSize) {
+
+                    inputFeature0.loadBuffer(byteBuffer)
+                } else {
+                    // Handle the size mismatch, log an error, or take appropriate action
+                    Log.e("TensorFlow", "Size bytebuffer ${byteBuffer}")
+                    Log.e("TensorFlow", "Size Tensorbuffer ${inputFeature0.flatSize}")
+                }
+
+
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+                val canvas = Canvas(bitmap)
+
+                val h = bitmap.height
+                val w = bitmap.width
+                paint.textSize = h / 15f
+                paint.strokeWidth = h / 85f
+
+                outputFeature0.forEachIndexed { x, fl ->
+                    val index = x * 4
+                    if (fl > 0.5) {
+                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(
+                            RectF(
+                                outputFeature0.get(index + 1) * w,
+                                outputFeature0.get(index) * h,
+                                outputFeature0.get(index + 3) * w,
+                                outputFeature0.get(index + 2) * h
+                            ), paint
+                        )
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(
+                            labels.get(
+                                outputFeature0.get(index).toInt()
+                            ) + " " + fl.toString(),
+                            outputFeature0.get(index + 1) * w,
+                            outputFeature0.get(index) * h,
+                            paint
+                        )
+                    }
+                }
+
+                imageView.setImageBitmap(bitmap)
 
 
             }
@@ -82,48 +148,63 @@ class CameraActivity : AppCompatActivity() {
     }
 
     @SuppressLint("MissingPermission")
-    fun open_camera(){
-        cameraManager.openCamera(cameraManager.cameraIdList[0], object: CameraDevice.StateCallback(){
-            override fun onOpened(p0: CameraDevice) {
-                cameraDevice = p0
+    fun open_camera() {
+        cameraManager.openCamera(
+            cameraManager.cameraIdList[0],
+            object : CameraDevice.StateCallback() {
+                override fun onOpened(p0: CameraDevice) {
+                    cameraDevice = p0
 
-                var surfaceTexture = textureView.surfaceTexture
-                var surface = Surface(surfaceTexture)
+                    var surfaceTexture = textureView.surfaceTexture
+                    var surface = Surface(surfaceTexture)
 
-                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                captureRequest.addTarget(surface)
+                    var captureRequest =
+                        cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                    captureRequest.addTarget(surface)
 
-                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback(){
-                    override fun onConfigured(p0: CameraCaptureSession) {
-                        p0.setRepeatingRequest(captureRequest.build(), null, null)
-                    }
-                    override fun onConfigureFailed(p0: CameraCaptureSession) {
-                    }
-                }, handler)
-            }
+                    cameraDevice.createCaptureSession(
+                        listOf(surface),
+                        object : CameraCaptureSession.StateCallback() {
+                            override fun onConfigured(p0: CameraCaptureSession) {
+                                p0.setRepeatingRequest(captureRequest.build(), null, null)
+                            }
 
-            override fun onDisconnected(p0: CameraDevice) {
+                            override fun onConfigureFailed(p0: CameraCaptureSession) {
+                            }
+                        },
+                        handler
+                    )
+                }
 
-            }
+                override fun onDisconnected(p0: CameraDevice) {
 
-            override fun onError(p0: CameraDevice, p1: Int) {
+                }
 
-            }
-        }, handler)
+                override fun onError(p0: CameraDevice, p1: Int) {
+
+                }
+            },
+            handler
+        )
     }
 
-    fun get_permission(){
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+    fun get_permission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
         }
     }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
             get_permission()
         }
     }
